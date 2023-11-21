c242df626ae9e9f218afb83c085852004d3e1e09
Lizcastro662 <lizcastro662@gmail.com>
2023-11-21 10:38:23 -0600

    cambios

--- practica 1.py
+++ practica 1.py
@@ -1,15 +1,13 @@
 import pandas as pd
 
-#Raw data
-df = pd.read_csv('roger-federer-data.csv')
-print("First Dataframe: ")
-print(df.head())
+file_path = "C:/Users/lizca/Downloads/base/roger-federer-data.csv"
+df = pd.read_csv (file_path)
 
-#Clean data
-col = ['player_ranking','tourney_year', 'tourney_name','tourney_location'
+df_clean = df [['player_ranking','tourney_year', 'tourney_name','tourney_location'
 ,'tourney_dates', 'tourney_conditions', 'tourney_surface'
-, 'opponent_name', 'match_win_loss','match_score']
-df1 = pd.read_csv('roger-federer-data.csv',usecols = col)
-print("Clean dataframe: ")
-df1.to_csv('rf-clean-data.csv')
-print(df1.head())
+, 'opponent_name', 'match_win_loss','match_score']]
+
+ruta_guardado = "C:/Users/lizca/Downloads/base/datoslimpios.csv"
+df_clean.to_csv (ruta_guardado, index= False)
+
+print(df_clean)
